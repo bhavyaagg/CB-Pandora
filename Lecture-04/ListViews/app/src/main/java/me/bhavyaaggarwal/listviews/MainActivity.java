086a47ee9c8d45f099fa7b53b6bb47bbcf719676
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-12-17 12:13:26 +0530

    L04: Set onItemClickListener in listviews
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture-04/ListViews/app/src/main/java/me/bhavyaaggarwal/listviews/MainActivity.java
+++ Lecture-04/ListViews/app/src/main/java/me/bhavyaaggarwal/listviews/MainActivity.java
@@ -2,8 +2,11 @@ package me.bhavyaaggarwal.listviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -43,5 +46,11 @@ public class MainActivity extends AppCompatActivity {
         );
 
         lvCourses.setAdapter(courseAdapter);
+        lvCourses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Clicked on " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
