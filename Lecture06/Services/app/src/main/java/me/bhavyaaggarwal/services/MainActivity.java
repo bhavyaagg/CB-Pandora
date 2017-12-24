93e4abbacf89d50762ba543a88e04d685af80e65
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-12-24 12:49:22 +0530

    L06: Use IntentService for working on a new thread
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture06/Services/app/src/main/java/me/bhavyaaggarwal/services/MainActivity.java
+++ Lecture06/Services/app/src/main/java/me/bhavyaaggarwal/services/MainActivity.java
@@ -1,26 +1,35 @@
 package me.bhavyaaggarwal.services;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnClick, btnStartService, btnStopService;
+    Button btnClick, btnStartService, btnStopService, btnChangeBK;
     TextView tvResult;
+    FrameLayout flMain;
+
+    Random r = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        flMain = findViewById(R.id.flMain);
         btnClick = findViewById(R.id.btnClick);
         btnStartService = findViewById(R.id.btnStartService);
         btnStopService = findViewById(R.id.btnStopService);
+        btnChangeBK = findViewById(R.id.btnChangeBK);
         tvResult = findViewById(R.id.tvResult);
 
         final Handler h = new Handler();
@@ -42,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
         btnStartService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, MyService.class);
+                Intent i = new Intent(MainActivity.this, MyIntentService.class);
                 startService(i);
             }
         });
@@ -50,11 +59,16 @@ public class MainActivity extends AppCompatActivity {
         btnStopService.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, MyService.class);
+                Intent i = new Intent(MainActivity.this, MyIntentService.class);
                 stopService(i);
             }
         });
 
-
+        btnChangeBK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flMain.setBackgroundColor(Color.rgb(r.nextInt(255), r.nextInt(255), r.nextInt(255)));
+            }
+        });
     }
 }
