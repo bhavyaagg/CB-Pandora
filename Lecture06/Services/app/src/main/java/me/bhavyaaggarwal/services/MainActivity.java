70255d11911a855fecf9a8c6fb2ba83701622259
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-12-24 12:31:21 +0530

    L06: Create Basic Service
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture06/Services/app/src/main/java/me/bhavyaaggarwal/services/MainActivity.java
+++ Lecture06/Services/app/src/main/java/me/bhavyaaggarwal/services/MainActivity.java
@@ -1,15 +1,17 @@
 package me.bhavyaaggarwal.services;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnClick;
+    Button btnClick, btnStartService, btnStopService;
     TextView tvResult;
 
     @Override
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnClick = findViewById(R.id.btnClick);
+        btnStartService = findViewById(R.id.btnStartService);
+        btnStopService = findViewById(R.id.btnStopService);
         tvResult = findViewById(R.id.tvResult);
 
         final Handler h = new Handler();
@@ -32,6 +36,22 @@ public class MainActivity extends AppCompatActivity {
                 };
                 tvResult.setText("");
                 h.postDelayed(r, 2000);
+            }
+        });
+
+        btnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                startService(i);
+            }
+        });
+
+        btnStopService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MyService.class);
+                stopService(i);
             }
         });
 
---
+++ Lecture06/Services/app/src/main/java/me/bhavyaaggarwal/services/MyService.java
@@ -0,0 +1,39 @@
+package me.bhavyaaggarwal.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+
+    public static final String TAG = "MyService";
+
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: ");
+        Log.d(TAG, "intent: " + intent);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
