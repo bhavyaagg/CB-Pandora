1f8b19a6999813b5d91d5196fbd83d04f95729ec
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-12-28 01:25:53 +0530

    L06: Assignment - Add code for MainActivity for TodoList in Kotlin
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture06/TodoListKotlin/app/src/main/java/me/bhavyaaggarwal/todolistkotlin/MainActivity.kt
+++ Lecture06/TodoListKotlin/app/src/main/java/me/bhavyaaggarwal/todolistkotlin/MainActivity.kt
@@ -2,11 +2,23 @@ package me.bhavyaaggarwal.todolistkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import me.bhavyaaggarwal.todolistkotlin.adapters.TodoRecyclerAdapter
+import me.bhavyaaggarwal.todolistkotlin.models.Todo
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvTodos.layoutManager = LinearLayoutManager(this);
+        val todoRecyclerAdapter: TodoRecyclerAdapter = TodoRecyclerAdapter(this);
+        rvTodos.adapter = todoRecyclerAdapter
+
+        btnAdd.setOnClickListener {
+            todoRecyclerAdapter.addTodo(Todo(etTask.text.toString(), false))
+        }
     }
 }
