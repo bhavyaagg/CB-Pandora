6290ce06cd0d5118372d85a7a99f73e0fbe30c09
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-12-28 01:05:12 +0530

    L06: Assignment - Add code for recycler view adapter in kotlin
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ Lecture06/TodoListKotlin/app/src/main/java/me/bhavyaaggarwal/todolistkotlin/adapters/TodoRecyclerAdapter.kt
@@ -0,0 +1,48 @@
+package me.bhavyaaggarwal.todolistkotlin.adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CompoundButton
+import kotlinx.android.synthetic.main.list_item_todo_item.view.*
+import me.bhavyaaggarwal.todolistkotlin.R
+import me.bhavyaaggarwal.todolistkotlin.models.Todo
+
+/**
+ * Created by bhavyaaggarwal on 27/12/17.
+ */
+class TodoRecyclerAdapter(val context: Context, val todos: ArrayList<Todo>) :
+        RecyclerView.Adapter<TodoRecyclerAdapter.TodoViewHolder>() {
+    override fun getItemCount(): Int {
+        return todos.size;
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder?, position: Int) {
+        holder?.bindView(todos[position]);
+        holder?.itemView!!.cbDone.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+            todos[position].done = isChecked
+        })
+        holder?.itemView!!.ivDelete.setOnClickListener(View.OnClickListener {
+            todos.removeAt(position)
+            notifyDataSetChanged()
+        })
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): TodoViewHolder {
+        val li: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater;
+        val itemView = li.inflate(R.layout.list_item_todo_item, parent, false);
+        return TodoViewHolder(itemView);
+    }
+
+
+    inner class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(todo: Todo) {
+            itemView.tvTask.text = todo.task
+            itemView.cbDone.isChecked = todo.done
+        }
+    }
+}
