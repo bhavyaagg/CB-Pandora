fad4a9f307e59f334daf6ac116432b558928e5d0
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-12-28 12:25:52 +0530

    L07: Ask for permissions dynamically
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture07/Permissions/app/src/main/java/me/bhavyaaggarwal/permissions/MainActivity.java
+++ Lecture07/Permissions/app/src/main/java/me/bhavyaaggarwal/permissions/MainActivity.java
@@ -1,17 +1,23 @@
 package me.bhavyaaggarwal.permissions;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -35,10 +41,18 @@ public class MainActivity extends AppCompatActivity {
         btnWriteFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
+                int perm = ContextCompat.checkSelfPermission(
+                        MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                if (perm == PackageManager.PERMISSION_GRANTED) {
                     writeFile();
-                } catch (IOException e) {
-                    Log.e(TAG, "Could not Write File", e);
+                } else {
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            44
+                    );
                 }
             }
         });
@@ -46,35 +60,76 @@ public class MainActivity extends AppCompatActivity {
         btnReadFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
+
+                int perm = ContextCompat.checkSelfPermission(
+                        MainActivity.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                if (perm == PackageManager.PERMISSION_GRANTED) {
                     tvFileData.setText(readFile());
-                } catch (IOException e) {
-                    Log.e(TAG, "Could not Read File", e);
+                } else {
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            45
+                    );
                 }
             }
         });
     }
 
-    void writeFile() throws IOException {
-        File sdCard = Environment.getExternalStorageDirectory();
-        File helloTxt = new File(sdCard, "hello.txt");
-        FileOutputStream foutStream = new FileOutputStream(helloTxt, true);
-        foutStream.write("Hello There\n".getBytes());
+    void writeFile() {
+        try {
+            File sdCard = Environment.getExternalStorageDirectory();
+            File helloTxt = new File(sdCard, "hello.txt");
+            FileOutputStream foutStream = new FileOutputStream(helloTxt, true);
+            foutStream.write("Hello There\n".getBytes());
+        } catch (IOException e) {
+            Log.e(TAG, "Could not Write File", e);
+        }
+
     }
 
-    String readFile() throws IOException {
-        File sdCard = Environment.getExternalStorageDirectory();
-        File helloTxt = new File(sdCard, "hello.txt");
-        FileInputStream finStream = new FileInputStream(helloTxt);
-        BufferedReader br = new BufferedReader(new InputStreamReader(finStream));
-        StringBuilder sb = new StringBuilder("");
-        String buf = br.readLine();
-        while (buf != null) {
-            sb.append(buf);
-            sb.append("\n");
-            buf = br.readLine();
-        }
-        return sb.toString();
+    String readFile() {
+        try {
+            File sdCard = Environment.getExternalStorageDirectory();
+            File helloTxt = new File(sdCard, "hello.txt");
+            FileInputStream finStream = new FileInputStream(helloTxt);
+            BufferedReader br = new BufferedReader(new InputStreamReader(finStream));
+            StringBuilder sb = new StringBuilder("");
+            String buf = br.readLine();
+            while (buf != null) {
+                sb.append(buf);
+                sb.append("\n");
+                buf = br.readLine();
+            }
+            return sb.toString();
 
+        } catch (IOException e) {
+            Log.e(TAG, "Could not Read File", e);
+            return "";
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == 44) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                writeFile();
+            } else {
+                Toast.makeText(this, "Writing file required this permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if (requestCode == 45) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                tvFileData.setText(readFile());
+            } else {
+                Toast.makeText(this, "Reading file required this permission", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
