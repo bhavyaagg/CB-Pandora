4440ac3f5f3bca965060dfe607be776ef2eac549
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-12-30 11:05:22 +0530

    L08: Networking App
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ Lecture08/Network/app/src/main/java/me/bhavyaaggarwal/network/MyAsyncTask.java
@@ -0,0 +1,14 @@
+package me.bhavyaaggarwal.network;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by bhavyaaggarwal on 30/12/17.
+ */
+
+public class MyAsyncTask extends AsyncTask<Integer, Integer, Void> {
+    @Override
+    protected Void doInBackground(Integer... integers) {
+        return null;
+    }
+}
