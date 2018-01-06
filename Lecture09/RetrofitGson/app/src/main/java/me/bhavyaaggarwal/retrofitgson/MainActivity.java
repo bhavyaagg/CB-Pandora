24e2787954e8531c36ce0a6fcdf51fb870f971ac
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-06 11:51:30 +0530

    L09: Get postById
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture09/RetrofitGson/app/src/main/java/me/bhavyaaggarwal/retrofitgson/MainActivity.java
+++ Lecture09/RetrofitGson/app/src/main/java/me/bhavyaaggarwal/retrofitgson/MainActivity.java
@@ -2,12 +2,45 @@ package me.bhavyaaggarwal.retrofitgson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import me.bhavyaaggarwal.retrofitgson.models.Post;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MAIN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Retrofit r = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .build();
+
+        JsonPlaceholder api = r.create(JsonPlaceholder.class);
+
+        api.getPostById(1).enqueue(new Callback<Post>() {
+            @Override
+            public void onResponse(Call<Post> call, Response<Post> response) {
+                Post p = response.body();
+                Log.d(TAG, "onResponse: " + p.getUserId());
+                Log.d(TAG, "onResponse: " + p.getId());
+                Log.d(TAG, "onResponse: " + p.getTitle());
+                Log.d(TAG, "onResponse: " + p.getBody());
+            }
+
+            @Override
+            public void onFailure(Call<Post> call, Throwable t) {
+
+            }
+        });
     }
 }
