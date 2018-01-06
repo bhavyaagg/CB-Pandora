ca40b8c1424a8ad18664b985cfa09c7251189d44
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-06 13:24:03 +0530

    L09: Dynamic Broadcast Receiver
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture09/BroadcastReceivers/app/src/main/java/me/bhavyaaggarwal/broadcastreceivers/MainActivity.java
+++ Lecture09/BroadcastReceivers/app/src/main/java/me/bhavyaaggarwal/broadcastreceivers/MainActivity.java
@@ -1,13 +1,62 @@
 package me.bhavyaaggarwal.broadcastreceivers;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout flBackground;
+    ChargeReceiver cr;
+    IntentFilter chargeFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flBackground = findViewById(R.id.flBackground);
+        cr = new ChargeReceiver();
+        chargeFilter = new IntentFilter();
+        chargeFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        chargeFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(cr, chargeFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(cr);
+        super.onPause();
+    }
+
+    public class ChargeReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent == null) {
+                return;
+            }
+
+            if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {
+                flBackground.setBackgroundColor(Color.GREEN);
+                Toast.makeText(context, "CHARGER CONNECTED", Toast.LENGTH_SHORT).show();
+            }
+
+            if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {
+                flBackground.setBackgroundColor(Color.RED);
+                Toast.makeText(context, "CHARGER DISCONNECTED", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
