1b72f46d39a509580b997ed6f7f4ae8c2a1cc554
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-06 10:54:44 +0530

    L08: Update
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture08/Network/app/src/main/java/me/bhavyaaggarwal/network/PostsActivity.java
+++ Lecture08/Network/app/src/main/java/me/bhavyaaggarwal/network/PostsActivity.java
@@ -1,13 +1,79 @@
 package me.bhavyaaggarwal.network;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import me.bhavyaaggarwal.network.models.Post;
 
 public class PostsActivity extends AppCompatActivity {
+    ListView lvPosts;
+    ArrayList<Post> postArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_posts);
+        lvPosts = findViewById(R.id.lvPosts);
+    }
+
+    class PostAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return postArrayList.size();
+        }
+
+        @Override
+        public Post getItem(int position) {
+            return postArrayList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(
+                        android.R.layout.simple_list_item_2,
+                        parent,
+                        false);
+            }
+
+            Post post = getItem(position);
+
+            ((TextView) convertView.findViewById(android.R.id.text1)).setText(post.getTitle());
+            ((TextView) convertView.findViewById(android.R.id.text2)).setText(post.getBody());
+
+            return null;
+        }
+    }
+
+    public class DownloadPosts extends AsyncTask<Void, Void, ArrayList<Post>> {
+
+        @Override
+        protected ArrayList<Post> doInBackground(Void... voids) {
+
+            try {
+                URL url = new URL("https://jsonplaceholder.typicode.com/posts");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
     }
 }
--- Lecture08/Network/app/src/main/java/me/bhavyaaggarwal/network/models/Post.java
+++ Lecture08/Network/app/src/main/java/me/bhavyaaggarwal/network/models/Post.java
@@ -1,51 +1,56 @@
 package me.bhavyaaggarwal.network.models;
 
-public class Post{
-	private int id;
-	private String title;
-	private String body;
-	private int userId;
+public class Post {
+    public static final String ID = "id";
+    public static final String TITLE = "title";
+    public static final String BODY = "body";
+    public static final String USER_ID = "userId";
 
-	public void setId(int id){
-		this.id = id;
-	}
+    private int id;
+    private String title;
+    private String body;
+    private int userId;
 
-	public int getId(){
-		return id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setTitle(String title){
-		this.title = title;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getTitle(){
-		return title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public void setBody(String body){
-		this.body = body;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public String getBody(){
-		return body;
-	}
+    public void setBody(String body) {
+        this.body = body;
+    }
 
-	public void setUserId(int userId){
-		this.userId = userId;
-	}
+    public String getBody() {
+        return body;
+    }
 
-	public int getUserId(){
-		return userId;
-	}
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 
-	@Override
- 	public String toString(){
-		return 
-			"Post{" + 
-			"id = '" + id + '\'' + 
-			",title = '" + title + '\'' + 
-			",body = '" + body + '\'' + 
-			",userId = '" + userId + '\'' + 
-			"}";
-		}
+    public int getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Post{" +
+                        "id = '" + id + '\'' +
+                        ",title = '" + title + '\'' +
+                        ",body = '" + body + '\'' +
+                        ",userId = '" + userId + '\'' +
+                        "}";
+    }
 }
