32dc0805571d89ab8e1fc592e4eccec9a7302a55
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-07 12:46:57 +0530

    L10: Code for adding a Todo and saving in DB
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture10/TodoWithDB/app/src/main/java/me/bhavyaaggarwal/todowithdb/MainActivity.java
+++ Lecture10/TodoWithDB/app/src/main/java/me/bhavyaaggarwal/todowithdb/MainActivity.java
@@ -4,9 +4,14 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
+import me.bhavyaaggarwal.todowithdb.adapters.TodoAdapter;
 import me.bhavyaaggarwal.todowithdb.db.TodoDatabaseHelper;
 import me.bhavyaaggarwal.todowithdb.db.tables.TodoTable;
 import me.bhavyaaggarwal.todowithdb.models.Todo;
@@ -15,19 +20,34 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "TODOS";
 
+    ListView lvTodos;
+    EditText etTask;
+    Button btnAdd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lvTodos = findViewById(R.id.lvTodos);
+        etTask = findViewById(R.id.etTask);
+        btnAdd = findViewById(R.id.btnAdd);
+
+
         TodoDatabaseHelper myDbHelper = new TodoDatabaseHelper(this);
-        SQLiteDatabase writeDb = myDbHelper.getWritableDatabase();
-        SQLiteDatabase readDb = myDbHelper.getReadableDatabase();
+        final SQLiteDatabase writeDb = myDbHelper.getWritableDatabase();
+        final SQLiteDatabase readDb = myDbHelper.getReadableDatabase();
 
-        TodoTable.insertTodo(new Todo(0, "One task", false), writeDb);
+        final TodoAdapter todoAdapter = new TodoAdapter(this, TodoTable.getAllTodos(readDb));
+        lvTodos.setAdapter(todoAdapter);
 
-        ArrayList<Todo> todos = TodoTable.getAllTodos(readDb);
-        Log.d(TAG, "onCreate: size = " + todos.size());
-        Log.d(TAG, "onCreate: 0 = " + todos.get(0).getTask());
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TodoTable.insertTodo(new Todo(0, etTask.getText().toString(), false), writeDb);
+                todoAdapter.refreshTodos(readDb);
+            }
+        });
     }
 }
