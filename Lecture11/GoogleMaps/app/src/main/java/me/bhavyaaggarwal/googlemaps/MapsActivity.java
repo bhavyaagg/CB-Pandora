1eb4c137050b2e343b213e55360a800aad8970ba
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-13 12:02:21 +0530

    L11: Move marker to Delhi
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture11/GoogleMaps/app/src/main/java/me/bhavyaaggarwal/googlemaps/MapsActivity.java
+++ Lecture11/GoogleMaps/app/src/main/java/me/bhavyaaggarwal/googlemaps/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng delhi = new LatLng(28.7, 77.1);
+        mMap.addMarker(new MarkerOptions().position(delhi).title("Marker in Delhi"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(delhi));
     }
 }
