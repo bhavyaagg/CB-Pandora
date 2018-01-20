692b89d312e8cac4b4d5118d1e29ca2c78be3925
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-20 11:33:05 +0530

    L13: Kotlin - Add function to the String Class
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture13/KotlinAdvancedFeatures/src/extensions.kt
+++ Lecture13/KotlinAdvancedFeatures/src/extensions.kt
@@ -1,10 +1,15 @@
-
 // RHS is returned from this function
 fun snakeCase(origString: String): String =
         origString.toLowerCase().replace(" ", "_")
+
+// Adding a function to the String class
+fun String.makeSnakeCase(): String =
+        this.toLowerCase().replace(" ", "_")
 
 fun main(args: Array<String>) {
     println(
             snakeCase("This is a sentence")
     )
+
+    println("This is a sentence".makeSnakeCase())
 }
