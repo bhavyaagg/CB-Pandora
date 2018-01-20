18920fb821e58a037709ff5d0941232b014d40ba
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-20 12:37:50 +0530

    L13: Kotlin - Extend from a class
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture13/KotlinAdvancedFeatures/src/objects.kt
+++ Lecture13/KotlinAdvancedFeatures/src/objects.kt
@@ -1,7 +1,18 @@
 object MyObj {
-    val x = 10;
+    val x = 10
+}
+
+// By default all classes in Kotlin are final so can not be inherited from
+// Therefore we use open class
+open class X {
+    val a = 10
+}
+
+object MyObj2 : X() {
+    val b = 20
 }
 
 fun main(args: Array<String>) {
     println(MyObj.x)
+    println(MyObj2.b)
 }
