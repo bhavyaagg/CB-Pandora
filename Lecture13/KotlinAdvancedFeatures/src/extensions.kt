fad69755739b4f2d71e6b7321dcd12134b04adc9
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-20 11:47:53 +0530

    L13: Kotlin Static Bindings
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture13/KotlinAdvancedFeatures/src/extensions.kt
+++ Lecture13/KotlinAdvancedFeatures/src/extensions.kt
@@ -3,6 +3,9 @@ fun snakeCase(origString: String): String =
         origString.toLowerCase().replace(" ", "_")
 
 // Adding a function to the String class
+// Function's binding is static but is not run in static scope that is why
+// using this is allowed
+// Can not be called using class name is className.function()
 fun String.makeSnakeCase(): String =
         this.toLowerCase().replace(" ", "_")
 
@@ -12,4 +15,20 @@ fun main(args: Array<String>) {
     )
 
     println("This is a sentence".makeSnakeCase())
+}
+
+
+// Having both dynamic function and extension
+class C {
+    // Dynamic Function
+    fun foo() {
+        println("member") // This will be printed
+    }
+}
+
+// Static Binding
+// Has no value since dynamic functions are checked first
+// Only when the function signatures are same
+fun C.foo() {
+    println("Extension") // This will not be printed
 }
