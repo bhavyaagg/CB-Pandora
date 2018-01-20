6c2df4094671723613bd32e194a8f2686268f155
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-20 11:23:50 +0530

    L13: Kotlin Lambda Functions
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture13/KotlinAdvancedFeatures/src/hof.kt
+++ Lecture13/KotlinAdvancedFeatures/src/hof.kt
@@ -2,8 +2,19 @@ fun sayHello(getName: (Int) -> String) {
     println("Hello " + getName(10))
 }
 
+fun sayHello2(getFirstName: () -> String, getLastName: () -> String) {
+    println("Hello " + getFirstName() + getLastName())
+}
+
+
 fun main(args: Array<String>) {
+    // In Anonymous Functions, last line is returned automatically
     sayHello({ i ->
         "Bhavya " + i
     })
+
+
+    // Lambda Functions
+    sayHello2({ "Bhavya" }, { "Aggarwal" })
+
 }
