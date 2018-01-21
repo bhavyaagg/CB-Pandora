59bb3df200266b219dd3b0331afc9ed6f70303d7
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-21 11:14:42 +0530

    L14: Basic Alarm Manager
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture14/AlarmManager/app/src/main/java/me/bhavyaaggarwal/alarmmanager/MainActivity.kt
+++ Lecture14/AlarmManager/app/src/main/java/me/bhavyaaggarwal/alarmmanager/MainActivity.kt
@@ -1,12 +1,34 @@
 package me.bhavyaaggarwal.alarmmanager
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val i = Intent(this, MainActivity::class.java)
+        val pi = PendingIntent.getActivity(
+                this,
+                123,
+                i,
+                PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val am: AlarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        am.setRepeating(
+                AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + 1 * 60 * 1000,
+                60 * 1000,
+                pi
+        )
     }
 }
