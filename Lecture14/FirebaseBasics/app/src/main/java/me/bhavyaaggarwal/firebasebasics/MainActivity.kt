64d219ea9aede35aaf38d5245fe09478b61f23a7
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-21 12:10:00 +0530

    L14: Log Events on Firebase
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture14/FirebaseBasics/app/src/main/java/me/bhavyaaggarwal/firebasebasics/MainActivity.kt
+++ Lecture14/FirebaseBasics/app/src/main/java/me/bhavyaaggarwal/firebasebasics/MainActivity.kt
@@ -2,11 +2,36 @@ package me.bhavyaaggarwal.firebasebasics
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        val leftClickBundle = Bundle()
+        leftClickBundle.putString(FirebaseAnalytics.Param.ITEM_ID, "left")
+
+        val rightClickBundle = Bundle()
+        rightClickBundle.putString(FirebaseAnalytics.Param.ITEM_ID, "right")
+
+        btnLeft.setOnClickListener({
+            firebaseAnalytics.logEvent(
+                    FirebaseAnalytics.Event.SELECT_CONTENT,
+                    leftClickBundle
+            )
+        })
+
+        btnRight.setOnClickListener({
+            firebaseAnalytics.logEvent(
+                    FirebaseAnalytics.Event.SELECT_CONTENT,
+                    rightClickBundle
+            )
+        })
+
     }
 }
