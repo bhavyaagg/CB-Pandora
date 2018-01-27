1df913fd9176d3e4375ff4f09e75fdf58a2fcd86
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-27 11:27:49 +0530

    L15: Multiple Fragments
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ Lecture15/Fragments/app/src/main/java/me/bhavyaaggarwal/fragments/BlueFragment.java
@@ -0,0 +1,26 @@
+package me.bhavyaaggarwal.fragments;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by bhavyaaggarwal on 27/01/18.
+ */
+
+public class BlueFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_blank, container);
+        rootview.findViewById(R.id.flBackground).setBackgroundColor(Color.BLUE);
+        return rootview;
+    }
+}
