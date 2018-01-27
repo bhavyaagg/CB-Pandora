4cfa4c04ef03c09aa4bb6c97eda1e6fe5c8e3188
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-27 13:17:02 +0530

    L15: Update IconFragment
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture15/NavDrawer/app/src/main/java/me/bhavyaaggarwal/navdrawer/IconFragment.java
+++ Lecture15/NavDrawer/app/src/main/java/me/bhavyaaggarwal/navdrawer/IconFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 /**
@@ -14,34 +15,20 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class IconFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_ICON_RESID = "icon_resid";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int iconResId;
 
 
     public IconFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment IconFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static IconFragment newInstance(String param1, String param2) {
+    public static IconFragment newInstance(int iconResId) {
         IconFragment fragment = new IconFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_ICON_RESID, iconResId);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +37,7 @@ public class IconFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            iconResId = getArguments().getInt(ARG_ICON_RESID, R.drawable.ic_menu_camera);
         }
     }
 
@@ -59,7 +45,13 @@ public class IconFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_icon, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_icon, container, false);
+
+        ((ImageView) rootView.findViewById(R.id.ivIcon)).setImageDrawable(
+                getResources().getDrawable(iconResId)
+        );
+
+        return rootView;
     }
 
 }
