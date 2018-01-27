88c160461de644ac1e21e99b038017fc343d6a23
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-27 11:16:58 +0530

    L15: Basic Static Fragment
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture15/Fragments/app/src/main/java/me/bhavyaaggarwal/fragments/BlankFragment.java
+++ Lecture15/Fragments/app/src/main/java/me/bhavyaaggarwal/fragments/BlankFragment.java
@@ -18,6 +18,9 @@ public class BlankFragment extends Fragment {
             LayoutInflater inflater,
             @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+
+        View rootview = inflater.inflate(R.layout.fragment_blank, container);
+
+        return rootview;
     }
 }
