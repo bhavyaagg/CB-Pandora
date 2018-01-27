d82364b26e61febac2d13558a2c2e09dd0b62cb2
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-27 12:06:34 +0530

    L15: Make FragmentManager global
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- Lecture15/Fragments/app/src/main/java/me/bhavyaaggarwal/fragments/DynamicFragmentActivity.java
+++ Lecture15/Fragments/app/src/main/java/me/bhavyaaggarwal/fragments/DynamicFragmentActivity.java
@@ -1,19 +1,23 @@
 package me.bhavyaaggarwal.fragments;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class DynamicFragmentActivity extends AppCompatActivity implements View.OnClickListener {
 
+    FragmentManager fragMan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dynamic_fragment);
 
+        fragMan = getSupportFragmentManager();
+
         findViewById(R.id.btnBlue).setOnClickListener(this);
         findViewById(R.id.btnRed).setOnClickListener(this);
-
     }
 
 
@@ -21,13 +25,13 @@ public class DynamicFragmentActivity extends AppCompatActivity implements View.O
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnBlue:
-                getSupportFragmentManager()
+                fragMan
                         .beginTransaction()
                         .add(R.id.flContainer, new BlueFragment())
                         .commit();
                 break;
             case R.id.btnRed:
-                getSupportFragmentManager()
+                fragMan
                         .beginTransaction()
                         .add(R.id.flContainer, new RedFragment())
                         .commit();
